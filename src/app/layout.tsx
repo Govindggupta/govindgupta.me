d3e3aeb921290b7db32bf91714b635bd7f9e9863
Govind <govind12771277@gmail.com>
2026-02-16 01:57:16 +0530

    update layout for the main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} relative min-h-screen bg-black antialiased`}
       >
         <DotBackground/> 
-        <div className="relative z-10">{children}</div>
+        <div className="relative z-10 mx-auto md:max-w-3xl *:[[id]]:scroll-mt-22">{children}</div>
       </body>
     </html>
   );

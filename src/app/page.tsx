73e76c497e3f0a0309ca4ff2c193b0fdf63aaf51
Govind <govind12771277@gmail.com>
2026-02-17 02:39:51 +0530

    logos added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import mainLogo from "@/logos/white_thick_border.svg";
+
 export default function Home() {
   return (
-    <>
-      <div className="h-full w-full flex justify-center">Govind</div>
-    </>
+    <div className="h-full w-full flex flex-col items-center justify-center p-6">
+      <h1>Govind</h1>
+      <Image src={mainLogo} alt="Govind logo" priority />
+    </div>
   );
 }

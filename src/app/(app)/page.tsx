8d079531ca4ffa66257d12f0f2d6174923f2f35f
Govind <govind12771277@gmail.com>
2026-02-19 01:44:03 +0530

    check again

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center p-6">
+    <div className="h-full w-full flex flex-col items-center justify-center p-6 border-y border-subtle -ml-[50vw] -mr-[50vw] relative left-1/2 right-1/2 w-screen px-[calc(50vw-50%+0.5rem)]">
       <h1>Govind</h1>
     </div>
   );

f7effca8ffd851823c610b1c281af73a2bc581d5
Govindggupta <Govindggupta@users.noreply.github.com>
2026-03-06 19:35:06 +0000

    Deploy preview for PR 1

---
+++ pr-previews/1/_next/static/chunks/app/(app)/about/page-4a6847d87ac8238d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[120,152,219,561,694],{5677:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9927,23))}},e=>{e.O(0,[927,792,972,358],()=>e(e.s=5677)),_N_E=e.O()}]);
